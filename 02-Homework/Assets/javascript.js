e4d7d74fd494a07a7a68aded8eb1f8bb62fb88e0
Chris Curry <Chamilitary216@hotmail.com>
2021-01-03 21:30:46 -0500

    Testing first div

---
+++ 02-Homework/Assets/javascript.js
@@ -0,0 +1,6 @@
+// Displays the current Date and Time
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+// Saves inputed data
