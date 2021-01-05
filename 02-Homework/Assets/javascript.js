5473be3b6e22b7c51f2b5a555fb079ff80a1ecf8
Chris Curry <Chamilitary216@hotmail.com>
2021-01-04 23:54:59 -0500

    console logged save button on click event

--- 02-Homework/Assets/javascript.js
+++ 02-Homework/Assets/javascript.js
@@ -4,3 +4,11 @@ var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
 // Saves inputed data
+
+$(".saveBtn").on("click", function(){
+    console.log(.saveBtn)
+    var index = $(".saveBtn").index(this);
+    task[index] = $(this).parent().find(".taskItem").text();
+    localStorage.setItem("task", JSON.stringify(task));
+
+});
