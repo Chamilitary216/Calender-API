5abbfbd1755c74d1871e96eaf657398ab6c91f5c
Chris Curry <Chamilitary216@hotmail.com>
2021-01-05 02:12:35 -0500

    function figured out to determine timeblock status

--- 02-Homework/javascript.js
+++ 02-Homework/javascript.js
@@ -4,3 +4,30 @@ $("#currentDay").html(todayDate);
 
 // Save Button
 
+$(".saveBtn").on("click", function() {
+    var index = $("saveBtn").index(this);
+    tasks[index] = $(this).parent().find(".taskItem").text();
+ //saves to local storage   
+    localStorage.setItem("tasks" , JSON.stringify(tasks));
+
+});
+
+// This is how it determines the time block for present, past and future
+$.each(timeHour, function (i, hour) {
+    var hourID = parseInt($(this).attr("id"));
+    if (hourID === now) {
+        $(this).next().addClass("present");
+    } else if (hourID < now) {
+        $(this).next().addClass("past");
+    } else if (hourID > now) {
+        $(this).next().addClass("future");
+    }
+});
+
+var timeHour = $(".hour");
+var now = parseInt(moment().format("H"));
+
+//Current moment of the day to determine if time block is present, past or future.
+
+var timeBlock = $(".hour");
+var now = parseInt(moment().format("H"));
